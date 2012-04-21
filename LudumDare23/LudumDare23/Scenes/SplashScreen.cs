f6aae58438c22ecb126967edf16a1ac29372c724
Patrick T <patrick.trautmann@gmail.com>
2012-04-21 06:23:41 +0200

    repaired

--- LudumDare23/LudumDare23/Scenes/SplashScreen.cs
+++ LudumDare23/LudumDare23/Scenes/SplashScreen.cs
@@ -1,4 +1,5 @@
-﻿using DynaStudios;
+﻿using System.Diagnostics;
+using DynaStudios;
 
 namespace LudumDare23.Scenes
 {
@@ -10,6 +11,12 @@ namespace LudumDare23.Scenes
         //3 Seconds visible
         public int splashVisisble = 3;
 
+        //Private Vars
+        private Stopwatch _watch;
+
+        private long _currentTime;
+        private long _lastTime;
+
         public SplashScreen(Engine engine)
         {
             Engine = engine;
@@ -19,26 +26,31 @@ namespace LudumDare23.Scenes
         public void doRender()
         {
             //Render Splash here
+//             _currentTime = _watch.ElapsedMilliseconds;
+// 
+//             long timeDifference = _currentTime - _lastTime;
+// 
+//             //If more then 1 Second difference: Recalculate fps
+//             if (timeDifference > splashVisisble * 1000 && _lastTime != 0)
+//             {
+//                 //After 3 seconds switch to Main Menu
+//                 Engine.switchScene("mainMenu");
+//             }
+// 
+//             _lastTime = _currentTime;
 
-
-            if (true)
-            {
-                //After 3 seconds switch to Main Menu
-                Engine.switchScene("mainMenu");
-            }
-            
         }
 
         public void loadScene()
         {
-            
+
         }
 
         public void unloadScene()
         {
             //Unload Texture from GPU here
             Engine.Logger.Debug("SplashScreen Unload called");
-            
+
         }
     }
 }
