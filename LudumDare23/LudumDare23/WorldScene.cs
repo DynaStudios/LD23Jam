5ccf678fc789aeab3b9ae823535bbdc74ba5730a
Patrick T <patrick.trautmann@gmail.com>
2012-04-21 18:31:36 +0200

    WorldScene

--- LudumDare23/LudumDare23/WorldScene.cs
+++ LudumDare23/LudumDare23/WorldScene.cs
@@ -12,7 +12,6 @@ namespace LudumDare23
     public class WorldScene : IScene
     {
         public Engine Engine { get; set; }
-        //private Chunklet chunklet1;
         //private Room _room;
         private Player player;
 
@@ -30,7 +29,6 @@ namespace LudumDare23
             player.Position.z = -3.0;
             Engine.Camera.WorldObject = (IWorldObject) player;
             //_room = new Room(Path.Combine(DynaStudios.Utils.StreamTool.DIR, "Maps", "map.xml"), Engine.TextureManager);
-            //chunklet1 = new Chunklet(0, 0, 0);
 
             _abilityBarTextureId = Engine.TextureManager.getTexture(@"Images\Game\abilityBar.png");
 
@@ -41,13 +39,35 @@ namespace LudumDare23
             player.doMovement();
 
             //_room.render();
-            //chunklet1.render(camera);
+        
+            //Render GUI
+            renderGui();
+
         }
 
         private void renderGui()
         {
             //Render Ability Bar
-            
+            GL.LoadIdentity();
+            GL.BindTexture(TextureTarget.Texture2D, _abilityBarTextureId);
+
+            //Ability Bar Vars
+            int texW = 800;
+            int texH = 80;
+
+            float translateX = ((Engine.Width / 2) - (texW / 2)) / Engine.Width;
+
+            GL.Translate(translateX, 0, 0);
+
+            GL.Begin(BeginMode.Quads);
+
+            GL.TexCoord2(0, 1); GL.Vertex3(-0.78f, -0.104f, 1.0f);
+            GL.TexCoord2(1, 1); GL.Vertex3(0.78f, -0.104f, 1.0f);
+            GL.TexCoord2(1, 0); GL.Vertex3(0.78f, 0.104f, 1.0f);
+            GL.TexCoord2(0, 0); GL.Vertex3(-0.78f, 0.104f, 1.0f);
+
+            GL.End();
+
             //Render Ability Icons
 
             //Render Selected Ability
