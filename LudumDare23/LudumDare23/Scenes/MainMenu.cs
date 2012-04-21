702764acfc2eb4300027a9375014e22bbddb5c00
Patrick T <patrick.trautmann@gmail.com>
2012-04-21 05:56:05 +0200

    Added DynaStudios Splash Image

---
+++ LudumDare23/LudumDare23/Scenes/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DynaStudios;
+
+namespace LudumDare23.Scenes
+{
+    public class MainMenu : IScene
+    {
+
+        public Engine Engine { get; set; }
+
+        public MainMenu(Engine engine)
+        {
+            Engine = engine;
+            Engine.Logger.Debug("Loaded MainMenu");
+        }
+
+        public void doRender()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void loadScene()
+        {
+            //Load Textures here
+            throw new NotImplementedException();
+        }
+
+        public void unloadScene()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LudumDare23/LudumDare23/Scenes/SplashScreen.cs
@@ -0,0 +1,37 @@
+﻿using DynaStudios;
+
+namespace LudumDare23.Scenes
+{
+    public class SplashScreen : IScene
+    {
+
+        public Engine Engine { get; set; }
+
+        //3 Seconds visible
+        public int splashVisisble = 3;
+
+        public SplashScreen(Engine engine)
+        {
+            Engine = engine;
+            Engine.Logger.Debug("Loaded SplashScreen");
+        }
+
+        public void doRender()
+        {
+            //Render Splash here
+
+            
+        }
+
+        public void loadScene()
+        {
+            
+        }
+
+        public void unloadScene()
+        {
+            //Unload Texture from GPU here
+            
+        }
+    }
+}
