de5d01d29cf469a1160938300fceab4e1765f34c
Patrick T <patrick.trautmann@gmail.com>
2012-04-21 04:28:26 +0200

    Changed Class Name

---
+++ LudumDare23/LudumDare23/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DynaStudios;
+
+namespace LudumDare23
+{
+    public class Game : Engine
+    {
+        public Game() : base(System.Reflection.Assembly.GetExecutingAssembly().Location + "/Maps")
+        { 
+        }
+
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Run();
+        }
+
+    }
+}
