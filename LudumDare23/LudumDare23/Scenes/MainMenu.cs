2831fa6e026c927f7b248abb332ce8f133d12525
Patrick T <patrick.trautmann@gmail.com>
2012-04-21 17:03:53 +0200

    Finished Mainmenu

--- LudumDare23/LudumDare23/Scenes/MainMenu.cs
+++ LudumDare23/LudumDare23/Scenes/MainMenu.cs
@@ -3,6 +3,7 @@ using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 using QuickFont;
 using System;
+using OpenTK.Input;
 
 namespace LudumDare23.Scenes
 {
@@ -21,6 +22,26 @@ namespace LudumDare23.Scenes
             Engine.Logger.Debug("Loaded MainMenu");
         }
 
+        private float getCursorPosition()
+        {
+            float position = 0.0f;
+
+            switch (_cursorYPosition)
+            {
+                case 1:
+                    position = 0.27f;
+                    break;
+                case 2:
+                    position = 0.17f;
+                    break;
+                case 3:
+                    position = 0.06f;
+                    break;
+            }
+
+            return position;
+        }
+
         public void doRender()
         {
 
@@ -37,12 +58,10 @@ namespace LudumDare23.Scenes
             GL.End();
 
             //Render Cursor on his Position here
-
             GL.MatrixMode(MatrixMode.Modelview);
-            GL.Translate(50, 0, 0);
             GL.LoadIdentity();
             GL.BindTexture(TextureTarget.Texture2D, _cursorTexture);
-
+            GL.Translate(0.37, getCursorPosition(), 0);
 
             GL.Begin(BeginMode.Quads);
 
@@ -74,12 +93,61 @@ namespace LudumDare23.Scenes
             _cursorTexture = Engine.TextureManager.getTexture(@"Images\Game\menuselection.png");
             _backgroundTexture = Engine.TextureManager.getTexture(@"Images\Game\mainmenu.png");
 
+            Engine.InputDevice.Keyboard.KeyUp += new EventHandler<OpenTK.Input.KeyboardKeyEventArgs>(Keyboard_KeyUp);
+
+        }
+
+        void Keyboard_KeyUp(object sender, OpenTK.Input.KeyboardKeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                selectMenuEvent();
+            }
+            if (e.Key == Key.Up)
+            {
+                if (_cursorYPosition != 1)
+                {
+                    _cursorYPosition--;
+                }
+                else
+                {
+                    _cursorYPosition = 3;
+                }
+            }
+            if (e.Key == Key.Down)
+            {
+                if (_cursorYPosition != 3)
+                {
+                    _cursorYPosition++;
+                }
+                else
+                {
+                    _cursorYPosition = 1;
+                }
+            }
         }
 
         public void unloadScene()
         {
             GL.DeleteTexture(_backgroundTexture);
             GL.DeleteTexture(_cursorTexture);
+            Engine.InputDevice.Keyboard.KeyUp -= Keyboard_KeyUp;
+        }
+
+        public void selectMenuEvent()
+        {
+            switch (_cursorYPosition)
+            {
+                case 1:
+                    Engine.switchScene("mainWorld");
+                    break;
+                case 2:
+                    Engine.switchScene("about");
+                    break;
+                case 3:
+                    Engine.Close();
+                    break;
+            }
         }
 
     }
