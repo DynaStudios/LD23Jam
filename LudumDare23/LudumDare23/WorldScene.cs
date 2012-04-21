1cec155e57a51d86e248c37b2aa5798d6d4a7a2d
Arthur Landel <arthur@dyna-studios.com>
2012-04-21 07:27:01 +0200

    first try to use Room

--- LudumDare23/LudumDare23/WorldScene.cs
+++ LudumDare23/LudumDare23/WorldScene.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using DynaStudios.Blocks;
 
 namespace LudumDare23
 {
@@ -9,7 +11,7 @@ namespace LudumDare23
     {
         public Engine Engine { get; set; }
         //private Chunklet chunklet1;
-
+        private Room _room;
         private Player player;
 
         private Camera camera = new Camera();
@@ -29,7 +31,10 @@ namespace LudumDare23
             player = new Player(Engine.InputDevice);
             player.Position.z = -3.0;
             Engine.Camera.WorldObject = (IWorldObject) player;
-
+            Console.WriteLine("preRoom");
+            FileStream stream = new FileInfo("FirstTest.map").OpenRead;
+            room = new Room(stream, Engine.TextureManager);
+            Console.WriteLine("postRoom");
             //chunklet1 = new Chunklet(0, 0, 0);
 
         }
@@ -41,6 +46,7 @@ namespace LudumDare23
             //moves the camera
             camera.move();
 
+            room.render();
             //chunklet1.render(camera);
 
             // unmoves the camera for the next frame
